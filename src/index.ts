46e62985a1dd8dca0231a42d55f378507b504f52
YamazakiYusuke <yusuke38098@gmail.com>
2024-11-19 09:10:51 +0900

    53

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-import { serve } from '@hono/node-server'
+import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import { getCookie, setCookie } from 'hono/cookie';
 
 const app = new Hono();
 
 app.get('/', (c) => {
-  // ファーストパーティクッキーの設定
-  if (getCookie(c, 'firstPartyCookie') == null) {
-    setCookie(c, 'firstPartyCookie', 'value1', {
-      httpOnly: true,
-    });
-  }
-
-  // サードパーティークッキーを取得
-  // TODO: change image path
   return c.html(`
-    <h1>Welcome to hoge.com</h1>
-    <img src="http://localhost:3000/image" alt="Advertisement Image">
+    <h1>Client Fetch Example</h1>
+    <button id="fetchData">Fetch Data</button>
+    <pre id="output"></pre>
+
+    <script>
+        document.getElementById('fetchData').addEventListener('click', async () => {
+            try {
+                const response = await fetch('https://ebbc-2409-13-96a1-7500-3def-8e1e-e29e-6e78.ngrok-free.app/data', {
+                    method: 'POST'
+                });
+                const data = await response.json();
+                document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        });
+    </script>
   `);
 });
 
